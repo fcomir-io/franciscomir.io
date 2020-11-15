b5711b780286667e78163853ee6833ffedf1034d
Francisco Mir <contact@franciscomir.io>
2020-11-15 10:23:16 +0100

    Working with AllMdx instead of allMarkdown Query changed since 'slug' is not a field in MDX Query optimized, so only needed data is requested Minor changes due to Prettier

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,13 @@
-const path = require("path")
+const path = require("path");
 
 /** Creating a slug for each markdown file and adding it into the node */
 module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   /** We will work only with our markdown files */
   if (node.internal.type === "MarkdownRemark") {
     /** Extract file name, without extension ==> slug */
-    const slug = path.basename(node.fileAbsolutePath, ".md")
+    const slug = path.basename(node.fileAbsolutePath, ".mdx");
 
     //console.log("Slug: ", slug)
 
@@ -16,68 +16,68 @@ module.exports.onCreateNode = ({ node, actions }) => {
       node,
       name: "slug",
       value: slug,
-    })
+    });
   }
-}
+};
 
 /** Creating a new page for every markdown document */
 module.exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   /**  Get path to template post.js */
-  const postTemplate = path.resolve("./src/templates/post.js")
+  const postTemplate = path.resolve("./src/templates/post.js");
   /**  Get path to template tag.js */
-  const tagPage = path.resolve("./src/templates/tag.js")
+  const tagPage = path.resolve("./src/templates/tag.js");
 
   /** Get markdown data */
   // Query to get slugs and tags ==> Returns a promise
   const queryResult = await graphql(`
     query {
-      allMarkdownRemark {
+      allMdx {
+        totalCount
         edges {
           node {
+            id
             frontmatter {
+              title
               tags
             }
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
-  `)
+  `);
 
   /** graphql returns a promise
    *  After result is available we can process it */
 
   // Set for all available TAGs
-  const tagSet = new Set()
+  const tagSet = new Set();
 
   // Going through the posts
-  queryResult.data.allMarkdownRemark.edges.forEach(edge => {
-
+  queryResult.data.allMdx.edges.forEach((edge) => {
     // Update TAGs Set
     if (edge.node.frontmatter.tags) {
-      edge.node.frontmatter.tags.forEach(tag => {
-        tagSet.add(tag)
-      })
+      edge.node.frontmatter.tags.forEach((tag) => {
+        tagSet.add(tag);
+      });
     }
 
     // Create blog pages
     createPage({
       component: postTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
         // Information to pass down to the template
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
-    })
-  })
+    });
+  });
 
   // Create tag pages
-  const tagList = Array.from(tagSet)
-  tagList.forEach(tag => {
+  const tagList = Array.from(tagSet);
+  tagList.forEach((tag) => {
     createPage({
       path: `/tags/${tag}/`,
       component: tagPage,
@@ -85,6 +85,6 @@ module.exports.createPages = async ({ graphql, actions }) => {
         // Information to pass down to the template
         tag: tag,
       },
-    })
-  })
-}
+    });
+  });
+};
