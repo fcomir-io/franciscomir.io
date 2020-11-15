30f0148f984e6e48d24cf0b7920aaeb8651cf197
Francisco Mir <contact@franciscomir.io>
2020-11-15 10:21:39 +0100

    Including the MDX Provider in the wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,17 @@
-import React from 'react'
-import { ThemeProvider } from './src/context/ThemeContext'
+import React from "react";
+import { ThemeProvider } from "./src/context/ThemeContext";
+import { MDXProvider } from "@mdx-js/react";
+
+const components = {
+  h2: ({ children }) => <h2 style={{ color: "red" }}>{children}</h2>,
+  "p.inlineCode": (props) => (
+    <code {...props} style={{ backgroundColor: "lightgray" }}></code>
+  ),
+};
 
 /** To provide the root with the ThemeContext */
-export const wrapRootElement = ({ element }) => <ThemeProvider>{element}</ThemeProvider>
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider>
+    <MDXProvider components={components}>{element}</MDXProvider>
+  </ThemeProvider>
+);
