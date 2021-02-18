3bfe8a78c03c1be18edf311920d3b8fc81e0313c
Francisco Mir <contact@franciscomir.io>
2021-02-18 11:44:08 +0100

    Updating logo and favicon

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,36 +1,36 @@
 // React libraries
-import React from "react"
+import React from "react";
 // Components from Gatsby library
-import { Link } from "gatsby"
+import { Link } from "gatsby";
 // Context for Dark/Light Theme
-import ThemeContext from "../../context/ThemeContext"
+import ThemeContext from "../../context/ThemeContext";
 // CSS
-import styles_variables from "./navigation.scss"
+import styles_variables from "./navigation.scss";
 // Images
-import banner from "../../images/banner.jpg"
-import favicon from "../../images/favicon.png"
-import sun from "../../images/sun.svg"
-import moon from "../../images/moon.svg"
+import banner from "../../images/banner.jpg";
+import favicon from "../../images/favicon.png";
+import sun from "../../images/sun.svg";
+import moon from "../../images/moon.svg";
 
 class Navigation extends React.Component {
-  static contextType = ThemeContext
+  static contextType = ThemeContext;
 
   state = {
     scrolled: false,
     lastScrollPos: 0,
     scrollDirection: "up",
-  }
+  };
 
   componentDidMount() {
-    window.addEventListener("scroll", this.navOnScroll)
+    window.addEventListener("scroll", this.navOnScroll);
   }
 
   componentWillUnmount() {
-    window.removeEventListener("scroll", this.navOnScroll)
+    window.removeEventListener("scroll", this.navOnScroll);
   }
 
   navOnScroll = () => {
-    const scrollDirection = this.state.scrollDirection
+    const scrollDirection = this.state.scrollDirection;
 
     /** For DEBUG purposes
     console.log(
@@ -45,58 +45,60 @@ class Navigation extends React.Component {
 
     // Check scroll direction
     if (window.scrollY > this.state.lastScrollPos) {
-      this.setState({ scrollDirection: "up" })
+      this.setState({ scrollDirection: "up" });
     } else {
-      this.setState({ scrollDirection: "down" })
+      this.setState({ scrollDirection: "down" });
     }
 
     // Scrolling up ==> scrolled is true when image disappears
     if (scrollDirection === "up") {
       if (window.scrollY > styles_variables.scrollThreshold_Up) {
-        this.setState({ scrolled: true })
+        this.setState({ scrolled: true });
       } else {
-        this.setState({ scrolled: false })
+        this.setState({ scrolled: false });
       }
     }
     // Scrolling down ==> scrolled is false when image appears completely
     else if (scrollDirection === "down") {
       if (window.scrollY < styles_variables.scrollThreshold_Down) {
-        this.setState({ scrolled: false })
+        this.setState({ scrolled: false });
       } else {
-        this.setState({ scrolled: true })
+        this.setState({ scrolled: true });
       }
     }
 
     // Update last position of scroll Y
-    this.setState({ lastScrollPos: window.scrollY })
-  }
+    this.setState({ lastScrollPos: window.scrollY });
+  };
 
   render() {
-    const { scrolled } = this.state
-    const { menuLinks } = this.props
-    const theme = this.context
+    const { scrolled } = this.state;
+    const { menuLinks } = this.props;
+    const theme = this.context;
 
     return (
       <>
         <div className={theme.dark ? "dark" : ""}>
           <div className={scrolled ? "nav scroll" : "nav"}>
             <div className="nav-container">
-              <Link to="/">
-                <div className="brand">
+              <div className="presentation-card">
+                <Link to="/">
                   <img
                     src={favicon}
                     className="favicon"
                     alt="I don't know what"
                   />
-                  <span className="text">Francisco Mir</span>
-                </div>
-                <p className="motto">
-                  LET'S BUILD SOMETHING TOGETHER...
-                </p>
-              </Link>
-
+                </Link>
+                <Link to="/">
+                  <div className="brand">
+                    <span className="text">Francisco Mir</span>
+                    
+                  <p className="motto">LET'S BUILD SOMETHING TOGETHER...</p>
+                  </div>
+                </Link>
+              </div>
               <div className="menu-links">
-                {menuLinks.map(link => (
+                {menuLinks.map((link) => (
                   <Link key={link.name} to={link.link} activeClassName="active">
                     {link.name}
                   </Link>
@@ -123,8 +125,8 @@ class Navigation extends React.Component {
           </div>
         </div>
       </>
-    )
+    );
   }
 }
 
-export default Navigation
+export default Navigation;
